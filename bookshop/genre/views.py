e1ae6c8bcd4ae45ff918d0c407209a264882f7b1
Dzmitry.Shchadzinski <demitr@tut.by>
2020-06-16 02:22:54 +0300

    genre work

--- bookshop/genre/views.py
+++ bookshop/genre/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpRequest
-from django.urls import reverse
-from django.views.generic import CreateView, UpdateView, DeleteView, FormView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 
 from .forms import CreateGenreForm
@@ -9,21 +6,21 @@ from .models import Genre
 
 # Create your views here.
 class CreateGenre(CreateView):
-    template_name = 'edit_genre.html'
+    template_name = 'genre_edit.html'
     model = Genre
     form_class = CreateGenreForm
     success_url = "/genre"
 class ViewGenres(ListView):
-    #template_name = 'index.html'
+    template_name = 'genre_list.html'
     model = Genre
     paginate_by = 100
 class EditGenre(UpdateView):
-    template_name = 'edit_genre.html'
+    template_name = 'genre_edit.html'
     model = Genre
     success_url = "/genre"
     form_class = CreateGenreForm
 class DeleteGenre(DeleteView):
-    template_name = 'delete_genre.html'
-    model = Genre
-    form_class = CreateGenreForm
     success_url = "/genre"
+    model = Genre
+    template_name = 'genre_delete.html'
+    
