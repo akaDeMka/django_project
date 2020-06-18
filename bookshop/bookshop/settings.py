b56a2a5c6baa333b9af3ebc65e73b6c314e1f199
Dzmitry.Shchadzinski <demitr@tut.by>
2020-06-19 00:53:07 +0300

    requiments

--- bookshop/bookshop/settings.py
+++ bookshop/bookshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '34egs8*i5)b5^1=ek9naq_gkxf_58qli&kxmkm4^hn%j7#9)4d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['akademka.pythonanywhere.com']
+ALLOWED_HOSTS = ['akademka.pythonanywhere.com', '127.0.0.1']
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
