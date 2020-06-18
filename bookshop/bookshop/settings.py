682bec61d4a1aac701394fdf11754a5377f32ab5
Dzmitry.Shchadzinski <demitr@tut.by>
2020-06-18 23:28:34 +0300

    Ready to publish

--- bookshop/bookshop/settings.py
+++ bookshop/bookshop/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/akademka/bookshop/static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/akademka/bookshop/media'
