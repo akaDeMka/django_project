14a3e430cf4c5c7f9b5db57a3361947eb7b6e47d
Dzmitry.Shchadzinski <demitr@tut.by>
2020-07-12 16:17:10 +0300

    Carousel img

--- bookshop/bookshop/settings.py
+++ bookshop/bookshop/settings.py
@@ -30,7 +30,6 @@ DEBUG = True
 ALLOWED_HOSTS = ['akademka.pythonanywhere.com', '127.0.0.1']
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'static' , 'css')
 ]
 
@@ -130,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/akademka/bookshop/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
