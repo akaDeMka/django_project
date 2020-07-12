9a4f60201a2e92c90d43e0b5eb1231fefb762dde
Dzmitry.Shchadzinski <demitr@tut.by>
2020-07-12 16:10:10 +0300

    Carousel img

--- bookshop/main/models.py
+++ bookshop/main/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Carousel(models.Model):
     name=models.CharField(max_length=50, verbose_name="Название слайда")
     text=models.TextField (blank=True, null=True, verbose_name="Текст для отображения")
-    link = models.CharField(max_length=100, verbose_name="Ссылка перехода")
-    button = models.CharField(max_length=50, verbose_name="Название перехода", default = 'Подробнее ...')
+    image=models.ImageField(upload_to='carousel/', blank=True, null=True, verbose_name="картинка карусели")
+    link = models.CharField(max_length=100, verbose_name="Ссылка перехода" , blank=True, null=True)
+    button = models.CharField(max_length=50, verbose_name="Название перехода", blank=True, null=True, default = 'Подробнее ...')
 
     def __str__(self):
         return self.name
