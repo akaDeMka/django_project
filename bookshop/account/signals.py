f84e302882263c871d4e798a2304ecf5126c4413
Dzmitry.Shchadzinski <demitr@tut.by>
2020-07-24 17:03:09 +0300

    full auth !!! Works

--- bookshop/account/signals.py
+++ bookshop/account/signals.py
@@ -1,7 +1,15 @@
 from .models import Profile
+from django.contrib.auth.models import Group
 
-def create_profile(sender, instance, **kwargs):
+def create_profile(sender, instance, created, **kwargs):
     obj, created = Profile.objects.get_or_create(
         user=instance,
         defaults={},
     )
+    if created:
+        obj, created = Group.objects.get_or_create(
+        name='Customer',
+        defaults={},
+        )
+        my_group = Group.objects.get(name='Customer')
+        my_group.user_set.add(instance)
